b74cc225a5e22f739dc2c706bf1bb85381331d44
s21veidieva <s21veidieva@vea.lv>
2024-03-12 21:56:36 +0200

    Navigation update with go_router
    Changed Navigator navigation with go_router navigation
    Added BottomNavigationBarScaffold widget
    Added new home_view widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:mytraveljournal/services/auth/auth_service.dart';
 import 'package:mytraveljournal/views/add_new_trip_view.dart';
+import 'package:mytraveljournal/views/home_view.dart';
 import 'package:mytraveljournal/views/landing_view.dart';
 import 'package:mytraveljournal/views/login_view.dart';
 import 'package:mytraveljournal/views/register_view.dart';
 import 'package:mytraveljournal/views/trip_memory_view.dart';
 import 'package:mytraveljournal/views/verify_email_view.dart';
 import 'package:mytraveljournal/views/welcome_view.dart';
-
 import 'constants/routes.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+  final String initRoute;
+  await AuthService.firebase().initialize();
+  final user = AuthService.firebase().currentUser;
+  if (user != null) {
+    if (user.isEmailVerified) {
+      initRoute = '/home';
+    } else {
+      initRoute = '/verifyEmail';
+    }
+  } else {
+    initRoute = '/welcome';
+  }
+  final rootNavigatorKey = GlobalKey<NavigatorState>();
+  final shellNavigatorKey = GlobalKey<NavigatorState>();
+
+  /// The route configuration.
+  final GoRouter router = GoRouter(
+    initialLocation: initRoute,
+    navigatorKey: rootNavigatorKey,
+    routes: [
+      ShellRoute(
+          navigatorKey: shellNavigatorKey,
+          routes: [
+            GoRoute(
+              parentNavigatorKey: shellNavigatorKey,
+              path: '/home',
+              builder: (context, state) => const HomeView(),
+            ),
+          ],
+          builder: (context, state, child) {
+            return BottomNavigationBarScaffold(child: child);
+          }),
+      GoRoute(
+        parentNavigatorKey: rootNavigatorKey,
+        path: '/welcome',
+        builder: (context, state) => const WelcomeView(),
       ),
-      home: const HomePage(),
-      routes: {
-        welcomeRoute: (context) => const WelcomeView(),
-        loginRoute: (context) => const LoginView(),
-        registerRoute: (context) => const RegisterView(),
-        verifyEmailRoute: (context) => const VerifyEmailView(),
-        landingRoute: (context) => const LandingView(),
-        addNewTripRoute: (context) => const AddNewTripView(),
-        // tripMemoryRoute: (context) => const TripMemoryView(),
-      },
+      GoRoute(
+        parentNavigatorKey: rootNavigatorKey,
+        path: '/verifyEmail',
+        builder: (context, state) => const VerifyEmailView(),
+      ),
+    ],
+  );
+
+  runApp(
+    MaterialApp.router(
+      routerConfig: router,
     ),
   );
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class BottomNavigationBarScaffold extends StatefulWidget {
+  const BottomNavigationBarScaffold({super.key, this.child});
+
+  final Widget? child;
+
+  @override
+  State<BottomNavigationBarScaffold> createState() =>
+      _BottomNavigationBarScaffoldState();
+}
+
+class _BottomNavigationBarScaffoldState
+    extends State<BottomNavigationBarScaffold> {
+  int currentIndex = 0;
+
+  void changeTab(int index) {
+    switch (index) {
+      case 0:
+        context.go('/home');
+        break;
+      case 1:
+        context.go('/profile');
+        break;
+      default:
+        context.go('/home');
+        break;
+    }
+    setState(() {
+      currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: AuthService.firebase().initialize(),
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.done:
-            final user = AuthService.firebase().currentUser;
-            if (user != null) {
-              if (user.isEmailVerified) {
-                return const LandingView();
-              } else {
-                return const VerifyEmailView();
-              }
-            } else {
-              return const WelcomeView();
-            }
-          default:
-            return const CircularProgressIndicator();
-        }
-      },
+    return Scaffold(
+      body: widget.child,
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
+        ],
+        selectedIndex: currentIndex,
+        onDestinationSelected: (int index) {
+          changeTab(index);
+        },
+      ),
     );
   }
 }
